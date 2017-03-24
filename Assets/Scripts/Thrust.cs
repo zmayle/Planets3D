4c4307f3631d65983987b66e76cb541c4a378ea7
zmayle <ZachMayle@dhcp-ccc-7066.eduroam.cornell.edu>
2017-03-24 17:09:34 -0400

    Added thrusters and improved level 3
    I created a particle system to represent the ship’s thrusters and added
    it to the player ship for every scene. Also made level 3 a better
    tutorial level.

---
+++ Assets/Scripts/Thrust.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour {
+
+	private ParticleSystem ps;
+	private AudioSource clip;
+
+	// Use this for initialization
+	void Start () {
+		ps = this.GetComponent<ParticleSystem> ();
+		clip = this.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1")) {
+			ps.Play();
+			if (! clip.isPlaying) clip.Play ();
+		}
+	}
+}
