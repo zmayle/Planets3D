697a7b2877fdce2f8d5562a78cc0b89a5829ba3b
zmayle <ZachMayle@Zacharys-MacBook-Air.local>
2017-04-06 14:50:39 -0400

    Completed 20 levels, fixed border
    Finished a total of 20 levels, including ones with black holes. Fixed
    the problem where you could see the rocket trail through the screen
    border.

--- Assets/Scripts/Thrust.cs
+++ Assets/Scripts/Thrust.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class Thrust : MonoBehaviour {
 
 	private ParticleSystem ps;
-	private AudioSource clip;
 
 	// Use this for initialization
 	void Start () {
 		ps = this.GetComponent<ParticleSystem> ();
-		clip = this.GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButton ("Fire1")) {
 			ps.Play();
-			if (! clip.isPlaying) clip.Play ();
 		}
 	}
 }
